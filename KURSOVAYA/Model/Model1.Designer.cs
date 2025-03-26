02a67c846a9ca04b6a4b0eb0cd7801fa5104d10a
Анастасия Мусатова <musatik2016@ya.ru>
2025-03-26 23:18:58 +0300

    frf

--- KURSOVAYA/Model/Model1.Designer.cs
+++ KURSOVAYA/Model/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\musat\OneDrive\Рабочий стол\KURSOVAYA3\KURSOVAYA\Model\Model1.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\musat\source\repos\anastasia3103\KURSOVAYAITOG\KURSOVAYA\Model\Model1.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
